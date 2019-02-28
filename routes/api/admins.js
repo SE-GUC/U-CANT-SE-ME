3950e5bdb89e3c1a470bb7e5f159ee58fa3e8a99
ahmedshams99 <46957449+ahmedshams99@users.noreply.github.com>
2019-02-28 09:57:36 +0200

    Update admins.js

--- routes/api/admins.js
+++ routes/api/admins.js
@@ -65,6 +65,19 @@ router.put('/update/:id', (req, res) => {
 })
 
 
-
+router.delete('/joi', (req,res) => {
+    const adminID = req.body.adminID;
+    let adminExists = false;
+    for(let i=0;i<admins.length;i++)
+        if(admins[i].id===adminID)
+        {
+            admins.splice(i,1);
+            adminExists = true;
+            break;
+        }   
+    if(!adminExists)
+        return res.status(404).send({ error: "Admin doesnt exist" });
+    return res.json({ data: admins});
+});
 
 module.exports = router;
