07c036b1c10f34b1daca3825c0b8a565c0a3d88f
ZeyadKhattab <zeyad.khattab97@gmail.com>
2019-03-01 22:46:47 +0200

    integrated Investor and Notification

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 //Require Route Handlers
 const investors = require('./routes/api/investors');
+const notifications = require('./routes/api/notifications')
 
 
 const app = express();
@@ -16,7 +17,12 @@ app.get('/', (req, res) => res.send('HomePage'));
 
 //Use Route Handlers
 app.use('/api/investors', investors);
+app.use('/api/notifications', notifications);
 
-
+// Handling 404
+app.use((req, res) => {
+    res.status(404).send({err: 'We can not find what you are looking for'});
+ })
+ 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
