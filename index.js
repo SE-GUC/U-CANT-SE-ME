e5d5abcfeee31c147fa6a07ddcd469589e405d86
Omar Samy <omarsamy90@gmail.com>
2019-03-01 23:43:13 +0200

    Create, Read & Update External Entity

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 //Require Route Handlers
 const investors = require('./routes/api/investors');
-
+const externalEntities = require('./routes/api/externalEntities');
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.get('/', (req, res) => res.send('HomePage'));
 
 //Use Route Handlers
 app.use('/api/investors', investors);
-
+app.use('/api/externalEntities', external_entities)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
