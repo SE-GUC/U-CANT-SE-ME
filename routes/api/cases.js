93d1d4a41bd8010dd5525fb7d8b6071b31515fa9
fadi-saad <47076092+fadi-saad@users.noreply.github.com>
2019-03-02 22:45:24 +0200

    Update cases.js

--- routes/api/cases.js
+++ routes/api/cases.js
@@ -100,8 +100,8 @@ router.post("/", (req, res) => {
     return res
       .status(400)
       .send({ err: "There must be a currencyUsedForCapital" });
-  if (typeof currencyUsedForCapital)
-    return res.status(400).send({ err: "currencyUsedForCapital string" });
+  if (typeof currencyUsedForCapital !=='string')
+    return res.status(400).send({ err: "currencyUsedForCapital must be string" });
   if (!capital) return res.status(400).send({ err: "There must be a Capital" });
   if (typeof capital !== "number")
     return res.status(400).send({ err: "Capital must be number" });
