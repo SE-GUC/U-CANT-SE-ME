37b8a5376ccd87376ef3ff26d7174fb3bd19586f
Abdulrahman Muhammad <abdoronic@gmail.com>
2019-03-02 19:46:21 +0200

    Revert "added all work on Case , please check if something is wrong with inde…"

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require('express');
 
 //Require Route Handlers
 const investors = require('./routes/api/investors');
-const Reviewers = require('./routes/api/reviewers.js');
 const admins = require('./routes/api/admins');
-const companies = require('./routes/api/company.js');
-const cases = require('./routes/api/cases');
 const notifications = require('./routes/api/notifications');
+const companies = require('./routes/api/company.js');
+const Reviewers = require('./routes/api/reviewers.js');
 
 const app = express();
 
@@ -20,17 +19,15 @@ app.get('/', (req, res) => res.send('HomePage'));
 
 //Use Route Handlers
 app.use('/api/investors', investors);
-app.use('/api/reviewers', Reviewers);
 app.use('/api/admins', admins);
-app.use('/api/company', companies);
-app.use('/api/cases', cases);
+app.use('/api/company', companies)
 app.use('/api/notifications', notifications);
+app.use('/api/reviewers', Reviewers)
 
 // Handling 404
 app.use((req, res) => {
     res.status(404).send({err: 'We can not find what you are looking for'});
  })
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
