5dfd1e919cb0f5a26f9e34e844fb2244a82f1649
youssefdiaa <youssefdiaa.ingame18@gmail.com>
2019-03-02 07:06:37 +0300

    admin part added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 //Require Route Handlers
 const investors = require('./routes/api/investors');
-
+const admins = require('./routes/api/admins');
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.get('/', (req, res) => res.send('HomePage'));
 
 //Use Route Handlers
 app.use('/api/investors', investors);
-
+app.use('/api/admins', admins);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
