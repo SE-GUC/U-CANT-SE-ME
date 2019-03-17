24eeee70fad2da22c0f2ae95f43ffadfc83e810f
Ronic <abdoronic@gmail.com>
2019-03-17 14:12:18 +0200

    Company renamed to Compinies

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 //Require Route Handlers
 const investors = require('./routes/api/investors');
-const admins = require('./routes/api/admins');
-const notifications = require('./routes/api/notifications');
-const companies = require('./routes/api/company.js');
-const Reviewers = require('./routes/api/reviewers.js');
 const lawyers =require('./routes/api/lawyers');
-const externalEntities = require('./routes/api/externalEntities');
+const Reviewers = require('./routes/api/reviewers');
+const admins = require('./routes/api/admins');
 const cases = require('./routes/api/cases');
+const companies = require('./routes/api/companies');
+const notifications = require('./routes/api/notifications');
+const externalEntities = require('./routes/api/externalEntities');
 
 const app = express();
 
@@ -22,13 +22,13 @@ app.get('/', (req, res) => res.send('HomePage'));
 
 //Use Route Handlers
 app.use('/api/investors', investors);
-app.use('/api/admins', admins);
-app.use('/api/company', companies);
-app.use('/api/notifications', notifications);
-app.use('/api/reviewers', Reviewers);
 app.use('/api/lawyers', lawyers);
-app.use('/api/externalEntities', externalEntities);
+app.use('/api/reviewers', Reviewers);
+app.use('/api/admins', admins);
 app.use('/api/cases', cases);
+app.use('/api/companies', companies);
+app.use('/api/notifications', notifications);
+app.use('/api/externalEntities', externalEntities);
 
 // Handling 404
 app.use((req, res) => {
