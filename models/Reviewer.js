8866e709998651056a0d2405a0963155847c1bc9
ouda98 <abdelrahman.ouda@student.guc.edu.eg>
2019-03-21 01:32:36 +0200

    mongoDB

--- models/Reviewer.js
+++ models/Reviewer.js
@@ -1,12 +1,22 @@
-const uuid = require("uuid");
-class Reviewer {
-  constructor(userName, password, fullName, email) {
-    this.id = uuid.v4();
-    this.userName = userName;
-    this.password = password;
-    this.fullName = fullName;
-    this.email = email;
-  }
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ReviewerSchema= new Schema({
+userName: {
+type:String,
+unique:true,
+required: true
+},
+password: {
+type: String,
+required: true
+},
+fullName: {
+type: String,
+required: true
+},
+email: {
+type:String,
+required:true
 }
-
-module.exports = Reviewer;
+});
+module.export = Reviewer = mongoose.model ('reviewers', ReviewerSchema)
