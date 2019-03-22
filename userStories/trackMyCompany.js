98ff285e9cdc8ffcf29c9d64689613de2e606520
youssefdiaa <youssefdiaa.ingame18@gmail.com>
2019-03-22 21:22:09 +0300

    Update trackMyCompany.js

--- userStories/trackMyCompany.js
+++ userStories/trackMyCompany.js
@@ -2,19 +2,27 @@
 const express = require("express");
 const mongoose = require('mongoose')
 const router = express.Router();
+const mongoValidator = require("validator");
 
 // Models
-const Case = require("../../models/Case");
+const Case = require("../models/Case.js");
+
 
 router.get('/:id', async (req,res) => {
+  
+  if(!mongoValidator.isMongoId(req.params.id))
+  return res.status(400).send({ err : "Invalid Investor Id" })
+
   const cases = await Case.find()
+
   const creatorInvestorId = req.params.id;
     flag = 0;
     message="";
     for (let i = 0; i < cases.length; i++) {
      
       if (String(cases[i].creatorInvestorId) === creatorInvestorId){  
-          message+="Your company "+cases[i].companyNameEnglish+" is currently in phase "+cases[i].caseStatus+" ";
+        // slash is added for now as delimiter so we can separate them in front end later  
+        message+="/ Your company "+cases[i].companyNameEnglish+" is currently in phase "+cases[i].caseStatus+" ";
           flag=1;
       }
     }
