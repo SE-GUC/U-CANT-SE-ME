0e5a508ecf20d820b09f6564924045b5c4079721
ahmedshams99 <46957449+ahmedshams99@users.noreply.github.com>
2019-03-22 23:11:49 +0200

    Update reviewerActions.js

--- userStories/reviewerActions.js
+++ userStories/reviewerActions.js
@@ -1,19 +1,29 @@
 const express = require("express")
 const router = express.Router()
-
+const Joi = require('joi')
 const Case = require("../models/Case.js")
+const mongoValidator = require("validator");
 const reviewerAutenticated=true;
 //in the future it will be assigned by calling some sort of method that authenticates them
 
 //Reviewer
-router.put('/updateCaseStatus/:caseID/:newStatus', async (req,res) => 
+router.put('/updateCaseStatus/:caseId/:caseStatus', async (req,res) => 
 {
     if(reviewerAutenticated)
     {
+        if(!mongoValidator.isMongoId(req.params.caseId))
+            return res.status(400).send({ err : "Invalid case id" })
+        const result = Joi.validate(req.params.caseStatus, 
+        {
+            caseStatus: Joi.string().required().valid(["New","OnUpdate", "WaitingForLawyer", "AssignedToLawyer", "WaitingForReviewer", "AssignedToReviewer", "Rejected", "Accepted"])
+        }
+        ); 
+        if (result.error)
+            return res.status(400).send({err: "Invalid new status"})
         try
         {
-            await Case.findByIdAndUpdate(req.params.caseID,{"caseStatus":req.params.newStatus})
-            res.json(await Case.findById(req.params.caseID))
+            await Case.findByIdAndUpdate(req.params.caseId,{"caseStatus":req.params.caseStatus})
+            res.json(await Case.findById(req.params.caseId))
         }
         catch(error)
         {
