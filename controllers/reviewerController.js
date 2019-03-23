a9e4d8107a0544633d11f5cb3a5636f6344d2904
yahiabadr <yahya.hisham97@gmail.com>
2019-03-23 18:11:25 +0200

    Update reviewerController.js

--- controllers/reviewerController.js
+++ controllers/reviewerController.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const validator = require('../../validations/reviewerValidations')
+const validator = require('../validations/reviewerValidations')
 
 var mongoValidator = require('validator')
 const Reviewer = require('../models/Reviewer');
