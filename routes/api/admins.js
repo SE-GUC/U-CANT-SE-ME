d6ab85d5dd7334a8dc3c151589dd4acc40432c7b
dondo98 <youssefdondo98@gmail.com>
2019-03-23 17:56:48 +0200

    adminController
    adminController

--- routes/api/admins.js
+++ routes/api/admins.js
@@ -1,81 +1,46 @@
 // Dependencies
+// Dependencies
 const express = require("express");
-const mongoose = require('mongoose')
 const router = express.Router();
-const validator = require('../../validations/adminValidations')
 
-// Models
-const Admin = require("../../models/Admin");
-
-// Get certain admin
-router.get("/:id",async (req, res) => {
-  const adminID = req.params.id;
-  const neededAdmin = await Admin.findById(adminID)
-  res.json({ data: neededAdmin})
-});
-
-
-// Get all admins
-router.get('/', async (req,res) => {
-  const admins = await Admin.find()
-  res.json({ data: admins })
-})
-
-
-// create admins
-router.post('/joi', async (req,res) => {
-  try {
-  
-     const isValidated = validator.createValidation(req.body)
-     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-  
-   const newAdmin = await Admin.create(req.body)
-   res.json({msg:'Admin was created successfully', data: newAdmin})
-  }
-  catch(error) {
-      // We will be handling the error later
-      console.log(error)
-  }  
-})
+// module Lawyer Controller
+const adminController = require("../../controllers/adminController");
 
 
 
+//Read
+router.get('/',adminController.getAllAdmins);
 
-// Update admin
-router.put("/update/:id", async (req,res) => {
-  try {
+router.get("/:id", adminController.getAdmin);
 
-    const id = req.params.id;
-    const {  userName, fullName, password }  = req.body
-   
-    const isValidated = validator.updateValidation(req.body)
-    if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-    
-   const admin = await Admin.findByIdAndUpdate(id,{  userName, fullName, password })
-   if(!admin) return res.status(404).send({error: 'admin does not exist'})
-  
-  
-   res.json({msg: 'Admin updated successfully'})
-  }
-  catch(error) {
-      // We will be handling the error later
-      console.log(error)
-  }  
-})
+//Create
+router.post('/joi', adminController.createAdmin);
+
+//Update
+router.put("/update/:id", adminController.updateAdmin);
+
+//Delete
+router.delete("/joi/:id", adminController.deleteAdmin);
 
 
-
-
-router.delete("/joi/:id", async (req,res) => {
-  try {
-    const adminID = req.params.id;
-   const deletedAdmin = await Admin.findByIdAndRemove(adminID)
-   res.json({msg:'Admin was deleted successfully', data: deletedAdmin})
-  }
-  catch(error) {
-      // We will be handling the error later
-      console.log(error)
-  }  
-})
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
