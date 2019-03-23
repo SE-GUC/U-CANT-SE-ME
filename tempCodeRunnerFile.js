fe35a9b94a577739b2e9dee261aec4f0d5609200
youssefdiaa <35878299+youssefdiaa@users.noreply.github.com>
2019-03-23 15:49:41 +0300

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-app.use("/api/reviewerActions", reviewerActions);
-// app.use("/api/lawyerActions", lawyerActions);
-// app.use("/api/lawyerGettingAllCases",lawyerGetAllCases);
-// app.use("/api/adminGettingAllCases",adminGetAllCases);
-// app.use("/api/reviewerGettingAllCases",reviewerGetAllCases);
