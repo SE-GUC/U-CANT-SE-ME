2a9f93c4ee2d268ff4b46a5c20579d49608a10a8
ZeyadKhattab <zeyad.khattab97@gmail.com>
2019-03-28 23:01:52 +0200

    Added more test functions

--- test.js
+++ test.js
@@ -30,7 +30,7 @@ test("Get all notifications initial test", async () => {
 });
 
 test("Create a notification ", async () => {
-  expect.assertions(1);
+  expect.assertions(2);
  
   let intialData=await getAll("notifications");
  
@@ -51,7 +51,7 @@ test("Create a notification ", async () => {
     intialData.push(data.data);
     
    expect(intialData).toEqual(finalData); 
-  
+  expect(intialData.length).toBe(finalData.length)
 });
 
 test("Create a notification With an invalid email does not do anything", async () => {
@@ -79,4 +79,78 @@ test("Create a notification With an invalid email does not do anything", async (
    const finalData=await getAll("notifications");
    expect(intialData).toEqual(finalData); 
   
+});
+test("Create a Notification then delete it", async () => {
+  expect.assertions(0);
+
+  
+  const body =  {
+    "emailOfRecipient": "ohzora.tsubasa@gmail.com",
+      "message": "HASHIREEEE!!",
+      "caseID": "5c968d4f233b707e2a617bcb",
+       "recipientId": "5c9645f36e2ef06af84adccf"
+     
+  };
+
+  const res = await axios.post(createURL("notifications"), body);
+
+  await axios.delete(createURL("notifications", [res.data._id] ));
+
+});
+
+// UPDATE
+test("Update an investor initial test", async () => {
+  expect.assertions(1);
+  const oldMessage="I have reached a critical conclusion!!!"
+  const newMessage="But did you commit on what you have decided?? :( "
+  //The body of the request
+  let body =  {
+    "emailOfRecipient": "hamada.yel3ab@gmail.com",
+      "message": oldMessage,
+      "caseID": "5c968d4f233b707e2a617bcb",
+       "recipientId": "5c9645f36e2ef06af84adccf"
+     
+  };
+
+  //Create the Investor
+  let res = await axios.post(createURL("notifications"), body);
+
+  let createdNotification = res.data.data;
+  console.log(createdNotification)
+  body.message = newMessage;
+  createdNotification.message=newMessage
+  
+  
+
+  //Update the newly created investor and get it
+  
+  const { data } =  await axios.put(createURL("notifications", [createdNotification._id] ), body);
+  
+  //Delete the newly created investor
+  await axios.delete(createURL("notifications", [createdNotification._id] ));
+
+  //Check if it was updated
+  expect(data.data).toEqual(createdNotification);
+});
+//DELETE
+test("Delete an investor initial test", async () => {
+  expect.assertions(1);
+
+  //The body of the request
+  const body =  {
+        "emailOfRecipient": "boyce.avenue@gmail.com",
+          "message": "I hope you are having fun testing :)",
+          "caseID": "5c968d4f233b707e2a617bcb",
+           "recipientId": "5c9645f36e2ef06af84adccf"
+         
+      };
+  let res = await axios.post(createURL("notifications"), body);
+
+  let createdNotification = res.data.data;
+
+  await axios.delete(createURL("notifications", [createdNotification._id] ));
+
+  const { data } = await axios.get(createURL("notifications"));
+
+  expect(data).not.toContain(createdNotification);
 });
