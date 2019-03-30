6be942395444251bce0df0777dd7a40060da817b
Ronic <abdoronic@gmail.com>
2019-03-31 00:36:55 +0200

    Added initial crud Tests

--- Jest/investors.js
+++ Jest/investors.js
@@ -1,9 +1,34 @@
 const axios = require('axios')
 
 const investors = {
+    readAllInvestors: async () => {
+        return axios.get('http://localhost:3000/api/investors');
+    },
+    readInvestor: async (id) => {
+        return axios.get(`http://localhost:3000/api/investors/${id}`);
+    },
+    createInvestor: async (body) => {
+        return axios.post('http://localhost:3000/api/investors', body);
+    },
+    updateInvestor: async (id, body) => {
+        return axios.put(`http://localhost:3000/api/investors/${id}`, body);
+    },
+    deleteInvestor: async (id) => {
+        return axios.delete(`http://localhost:3000/api/investors/${id}`);
+    },
     login: async (loginInfo) => {
         return axios.post('http://localhost:3000/api/investors/login', loginInfo)
     }
 }
+
+async function httpRequest(method, urlSuffix, params = [], body = {}) {
+    let url = "http://localhost:3000/api/" + urlSuffix + "/";
+    for (let i = 0; i < params.length; i++) url += params[i] + "/";
+    if (method === "GET") return await axios.get(url);
+    else if (method === "POST") return await axios.post(url, body);
+    else if (method === "PUT") return await axios.put(url, body);
+    else if (method === "DELETE") return await axios.delete(url);
+    return {};
+  }
 
 module.exports = investors
--- Jest/investors.test.js
+++ Jest/investors.test.js
@@ -2,13 +2,126 @@
  * @jest-environment node
  */
 
-const investors = require('./investors')
+const investors = require("./investors");
 
-test('As an investor I should be able to login', async() => {
-    const loginInfo = {
-        email: 'scrummaster@gmail.com',
-        password: '12345678fea'
-    }
-    const loginResult = await investors.login(loginInfo)
-    return expect(loginResult.data.length).toBeGreaterThan(0)
-})  
+//READ
+test("Get all investors initial test", async () => {
+  expect.assertions(0);
+
+  //data is the rresponse object from the server
+  const { data } = await investors.readAllInvestors();
+
+  console.log(data);
+});
+
+//CREATE
+test("Create an investor initial test", async () => {
+  expect.assertions(0);
+
+  //The body of the request
+  const body = {
+    email: "meshabc@gmail.com",
+    password: "12345678",
+    fullName: "Abc Ibn Xyz",
+    type: "a",
+    gender: "Male",
+    nationality: "Egyptian",
+    methodOfIdentification: "National Card",
+    identificationNumber: "12233344445555",
+    dateOfBirth: "1990-12-17T22:00:00.000Z",
+    residenceAddress: "13th Mogama3 el Tahrir",
+    telephoneNumber: "00201009913457",
+    fax: "1234567"
+  };
+
+  const res = await investors.createInvestor(body);
+
+  console.log(res);
+
+  await investors.deleteInvestor(res.data._id);
+});
+
+// UPDATE
+test("Update an investor initial test", async () => {
+  expect.assertions(1);
+
+  //The body of the request
+  let body = {
+    email: "hossamelUpdated@gmail.com",
+    password: "12345678",
+    fullName: "Abc Ibn Xyz",
+    type: "a",
+    gender: "Male",
+    nationality: "Egyptian",
+    methodOfIdentification: "National Card",
+    identificationNumber: "12233344445555",
+    dateOfBirth: "1990-12-17T22:00:00.000Z",
+    residenceAddress: "13th Mogama3 el Tahrir",
+    telephoneNumber: "00201009913457",
+    fax: "1234567"
+  };
+
+  //Create the Investor
+  let res = await investors.createInvestor(body);
+
+  let createdInvestor = res.data;
+
+  createdInvestor.fullName = "Updated Name";
+  createdInvestor.fax = "71234234";
+
+  body = {
+    fullName: "Updated Name",
+    fax: "71234234"
+  };
+
+  //Update the newly created investor and get it
+  await investors.updateInvestor(createdInvestor._id, body);
+
+  const { data } = await investors.readInvestor(createdInvestor._id);
+
+  //Delete the newly created investor
+  await investors.deleteInvestor(createdInvestor._id);
+
+  //Check if it was updated
+  expect(data).toEqual(createdInvestor);
+});
+
+//DELETE
+test("Delete an investor initial test", async () => {
+  expect.assertions(1);
+
+  //The body of the request
+  let body = {
+    email: "hossamEldeleted@gmail.com",
+    password: "12345678",
+    fullName: "Abc Ibn Xyz",
+    type: "a",
+    gender: "Male",
+    nationality: "Egyptian",
+    methodOfIdentification: "National Card",
+    identificationNumber: "12233344445555",
+    dateOfBirth: "1990-12-17T22:00:00.000Z",
+    residenceAddress: "13th Mogama3 el Tahrir",
+    telephoneNumber: "00201009913457",
+    fax: "1234567"
+  };
+
+  let res = await investors.createInvestor(body);
+
+  let createdInvestor = res.data;
+
+  await investors.deleteInvestor(createdInvestor._id);
+
+  const { data } = await investors.readAllInvestors();
+
+  expect(data).not.toContain(createdInvestor);
+});
+
+test("As an investor I should be able to login", async () => {
+  const loginInfo = {
+    email: "scrummaster@gmail.com",
+    password: "12345678fea"
+  };
+  const loginResult = await investors.login(loginInfo);
+  return expect(loginResult.data.length).toBeGreaterThan(0);
+});
