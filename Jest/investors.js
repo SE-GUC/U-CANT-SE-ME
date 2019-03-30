2da06fc64e2b5c97e464ed2ebd9474b363c033ed
Mohamed Adel <moeadele98@gmail.com>
2019-03-30 20:14:10 +0200

    Updated Dependencies

--- Jest/investors.js
+++ Jest/investors.js
@@ -1,8 +1,17 @@
 const axios = require('axios')
 
 const investors = {
+    default: async () => {
+        return axios.get('http://localhost:3000/api/investors/')
+    },
+    registerInvestor: async (req) => {
+        return axios.post('http://localhost:3000/api/investors/register', req)
+    },
     login: async (loginInfo) => {
         return axios.post('http://localhost:3000/api/investors/login', loginInfo)
+    },
+    deleteInvestor: async(id) => {
+        return axios.delete(`http://localhost:3000/api/investors/${id}`)
     }
 }
 
