3cd98dbc91bc0a7dedcb4c8c2411fe9458982621
Mohamed Adel <moeadele98@gmail.com>
2019-03-30 21:19:12 +0200

    Test

--- Jest/investors.test.js
+++ Jest/investors.test.js
@@ -6,6 +6,7 @@ const investors = require('./investors')
 let createdEmail = ''
 let createdPassword = ''
 let investorId = ''
+let caseId = ''
 
 test('Create All Dependencies', async() => {
     const investor = {
@@ -26,6 +27,53 @@ test('Create All Dependencies', async() => {
     createdEmail = createdInvestor.data.data.email
     createdPassword = investor.password
     investorId = createdInvestor.data.data._id
+    const mycase =  {
+        form: {
+            companyType: 'SPC',
+            regulatedLaw: 'lll',
+            legalFormOfCompany: 'NonProfitMan',
+            companyNameArabic: 'شركة مو التجاريةMoeMan',
+            companyNameEnglish: 'Moe CompanyMoeee',
+            headOfficeGovernorate: 'California',
+            headOfficeCity: 'San Francisco',
+            headOfficeAddress: '123st.',
+            phoneNumber: '01007063067',
+            fax: '987654321',
+            currencyUsedForCapital: 'EGP',
+            capital: 100
+        },
+        caseStatus: 'WaitingForLawyer',
+        creatorInvestorId: investorId
+    }
+    const createdCase = await investors.createCase(mycase)
+    caseId = createdCase.data.data._id    
+    const updatedCase = {
+        form: {
+            companyType: 'SPC',
+            regulatedLaw: 'lll',
+            legalFormOfCompany: 'NonProfitMan',
+            headOfficeGovernorate: 'California',
+            headOfficeCity: 'San Francisco',
+            headOfficeAddress: '123st.',
+            phoneNumber: '01007063067',
+            fax: '987654321',
+            currencyUsedForCapital: 'EGP',
+            capital: 100
+        },
+        caseStatus: 'WaitingForLawyer',
+        comments:[
+            {
+                author:"Moe",
+                body:"Good Company!"
+                
+            },
+            {
+                author:"MoeMan",
+                body:"I second Moe"	
+            }
+        ]
+    }
+    await investors.updateCase(caseId, updatedCase)
 })    
 
 test('As an investor I should be able to login', async() => {
@@ -35,9 +83,9 @@ test('As an investor I should be able to login', async() => {
     }
     const loginResult = await investors.login(loginInfo)
     return expect(loginResult.data.length).toBeGreaterThan(0)
-})  
+}) 
 
 test('Delete All Dependencies', async () => {
-    console.log(investorId)
     await investors.deleteInvestor(investorId)
+    await investors.deleteCase(caseId)
 })
