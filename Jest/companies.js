717ef327f3cdeb9f5bc41a867debe46b26b69506
Moe98 <moeadele98@gmail.com>
2019-03-30 19:56:56 +0200

    Updated Dependencies
    To run the test first run the server
    `npm run dev`
    then run the test file
    `npm run test`

--- Jest/companies.js
+++ Jest/companies.js
@@ -15,6 +15,18 @@ const companies = {
     },
     deleteCompany: async (id) => {
         return axios.delete(`http://localhost:3000/api/companies/${id}`)
+    },
+    createInvestor: async (req)=>{
+        return axios.post('http://localhost:3000/api/investors', req)
+    },
+    deleteInvestor: async (id)=>{
+        return axios.delete(`http://localhost:3000/api/investors/${id}`)
+    },
+    createCase: async (req)=>{
+        return axios.post('http://localhost:3000/api/cases', req)
+    },
+    deleteCase: async (id)=>{
+        return axios.delete(`http://localhost:3000/api/cases/${id}`)
     }
 }
 
--- Jest/companies.test.js
+++ Jest/companies.test.js
@@ -3,28 +3,72 @@
  */
 
 const companies = require('./companies')
-
+let createdInvestor = {}
+let createdCase = {}
 let companyID = ''
+let caseId = ''
+let investorId = ''
 
-test('Get all companies', async() => {
-    const allCompanies = await companies.default()
-})
+
+test('Create All Dependencies', async() => {
+    const investor = {
+        email:"moe@gmail.com",
+        password:"dontusethispassword",
+        fullName:"MoeAdel",
+        type:"CEO",
+        gender:"Male",
+        nationality:"Egyptian",
+        methodOfIdentification:"National Card",
+        identificationNumber:"12233344445555",
+        dateOfBirth:"1997-12-15T22:00:00.000Z",
+        residenceAddress:"Rehab City",
+        telephoneNumber:"01007063067",
+        fax:"123456789"
+    }
+    createdInvestor  = await companies.createInvestor(investor)
+    investorId = createdInvestor.data._id
+    const mycase =  {
+        form: {
+            companyType: 'SPC',
+            regulatedLaw: 'lll',
+            legalFormOfCompany: 'NonProfit',
+            companyNameArabic: 'شركة مو التجارية',
+            companyNameEnglish: 'Moe Company',
+            headOfficeGovernorate: 'California',
+            headOfficeCity: 'San Francisco',
+            headOfficeAddress: '123st.',
+            phoneNumber: '01007063067',
+            fax: '987654321',
+            currencyUsedForCapital: 'EGP',
+            capital: 100
+        },
+        caseStatus: 'WaitingForLawyer',
+        creatorInvestorId: createdInvestor.data._id
+    }
+    createdCase  = await companies.createCase(mycase)
+    caseId = createdCase.data.data._id    
+})    
 
 
 test('Creating a company', async () => {
     const company = {
         socialInsuranceNumber: 189319,
-        investorID: '5c9d0eb6af006b31fca33641',
-        companyName: 'Test39',
+        investorID: investorId,
+        companyName: 'Moe Company',
         companyType: 'SPC',
         dateOfCreation: '1998/12/12',
-        caseID: '5c968d4f233b707e2a617bcb'
+        caseID: caseId
     }
     expect.assertions(1)
     const createdCompany = await companies.createCompany(company)
     const afterCreation = await companies.readCompany(createdCompany.data.data['_id'])
     companyID = createdCompany.data.data['_id']
     return expect(createdCompany.data.data).toEqual(afterCreation.data.data)
+})
+
+test('Get all companies', async() => {
+    const allCompanies = await companies.default()
+    return expect(allCompanies.data.data.length).toBeGreaterThan(0)
 })
 
 test('Get company by ID', async() => {
@@ -35,8 +79,7 @@ test('Get company by ID', async() => {
 
 test('Update Company Name', async () =>{
     const name = { newCompanyName: 'MantaHabd' }
-    const testFound = await companies.readCompany(companyID)
-    const updatedCompany = await companies.updateCompany(companyID, name)
+    await companies.updateCompany(companyID, name)
     const fetchCompany = await companies.readCompany(companyID)
     return expect(fetchCompany.data.data.companyName).toEqual(name.newCompanyName)
 })
@@ -44,6 +87,11 @@ test('Update Company Name', async () =>{
 test('Delete a company', async () => {
     expect.assertions(0)
     const findCompany = await companies.readCompany(companyID)
-    const deletedCompany = await companies.deleteCompany(companyID)
+    await companies.deleteCompany(companyID)
     return expect.not.arrayContaining(findCompany.data.data)
+})
+
+test('Delete All Dependencies', async () => {
+    await companies.deleteCase(caseId)
+    await companies.deleteInvestor(investorId)
 })
