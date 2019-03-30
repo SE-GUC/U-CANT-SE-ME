5aca1c53ba3c341c8590811008dfb03bd8d70ada
Moe98 <moeadele98@gmail.com>
2019-03-30 10:16:30 +0200

    Added Update Company Jest

--- Jest/companies.js
+++ Jest/companies.js
@@ -10,8 +10,8 @@ const companies = {
     readCompany: async (id) =>{
         return axios.get(`http://localhost:3000/api/companies/${id}`)
     },
-    updateCompany: async (name, newName) => {
-        return axios.put(`http://localhost:3000/api/companies/${name}`, newName)
+    updateCompany: async (id, newName) => {
+        return axios.put(`http://localhost:3000/api/companies/${id}`, newName)
     },
     deleteCompany: async (id) => {
         return axios.delete(`http://localhost:3000/api/companies/${id}`)
--- Jest/companies.test.js
+++ Jest/companies.test.js
@@ -10,18 +10,12 @@ test('Get all companies', async() => {
     const allCompanies = await companies.default()
 })
 
-test('Get company by ID', async() => {
-    expect.assertions(1)
-    let companyID = "5c9cf046c94f0f3410928fc3"
-    const companyByID = await companies.readCompany(companyID)
-    return expect(companyID).toEqual(companyByID.data.data['_id'])
-})
 
 test('Creating a company', async () => {
     const company = {
         socialInsuranceNumber: 189319,
-        investorID: '5c9cd88a482605e4c2d9c17e',
-        companyName: 'Test33',
+        investorID: '5c9d0eb6af006b31fca33641',
+        companyName: 'Test39',
         companyType: 'SPC',
         dateOfCreation: '1998/12/12',
         caseID: '5c968d4f233b707e2a617bcb'
@@ -31,6 +25,20 @@ test('Creating a company', async () => {
     const afterCreation = await companies.readCompany(createdCompany.data.data['_id'])
     companyID = createdCompany.data.data['_id']
     return expect(createdCompany.data.data).toEqual(afterCreation.data.data)
+})
+
+test('Get company by ID', async() => {
+    expect.assertions(1)
+    const companyByID = await companies.readCompany(companyID)
+    return expect(companyID).toEqual(companyByID.data.data['_id'])
+})
+
+test('Update Company Name', async () =>{
+    const name = { newCompanyName: 'MantaHabd' }
+    const testFound = await companies.readCompany(companyID)
+    const updatedCompany = await companies.updateCompany(companyID, name)
+    const fetchCompany = await companies.readCompany(companyID)
+    return expect(fetchCompany.data.data.companyName).toEqual(name.newCompanyName)
 })
 
 test('Delete a company', async () => {
