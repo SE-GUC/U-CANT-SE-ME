5a1c563edbe929fe13c1fd6ffc9106ee6bc0abe4
ahmedshams99 <46957449+ahmedshams99@users.noreply.github.com>
2019-03-31 21:26:12 +0200

    Update reviewers.test.js

--- Jest/reviewers.test.js
+++ Jest/reviewers.test.js
@@ -337,3 +337,74 @@ const caseCreated= await reviewers.createCase(form);
  await reviewers.deleteCase(caseCreated.data.data._id);
  await reviewers.deleteInvestor(investorCreated.data._id);
 });
+test('Change Status', async () => {
+    const investor = {
+    email:"shamsTesting21231@gmail.com",
+    password:"verystrongpassword",
+    fullName:"randomnametest",
+    type:"a",
+    gender:"Male",
+    nationality:"Egyptian",
+    methodOfIdentification:"National Card",
+    identificationNumber:"12233344445555",
+    dateOfBirth:"1990-12-17T22:00:00.000Z",
+    residenceAddress:"13th Mogama3 el Tahrir",
+    telephoneNumber:"00201009913457",
+    fax:"1234567"
+}
+const createdInvestor  = await reviewers.createInvestor(investor)
+const mycase =  {
+    form: {
+        companyType: 'SPC',
+        regulatedLaw: 'lll',
+        legalFormOfCompany: 'Moes3',
+        companyNameArabic: 'shamstest3453565',
+        companyNameEnglish: 'bardotest50647',
+        headOfficeGovernorate: 'Joes3',
+        headOfficeCity: 'Mantas3',
+        headOfficeAddress: 'Shamss3',
+        phoneNumber: '123456789',
+        fax: '987654321',
+        currencyUsedForCapital: 'EGP',
+        capital: 100
+    },
+    caseStatus: 'WaitingForLawyer',
+    creatorInvestorId: createdInvestor.data._id
+}
+const createdCase  = await reviewers.createCase(mycase)
+//Start of tests
+const caseID=createdCase.data.data._id
+
+const caseAccepted=await reviewers.changeStatus(caseID,'Accepted')
+expect(caseAccepted.data.caseStatus).toBe('Accepted')
+
+const caseRejected=await reviewers.changeStatus(caseID,'Rejected')
+expect(caseRejected.data.caseStatus).toBe('Rejected')
+    
+const caseOnUpdate=await reviewers.changeStatus(caseID,'OnUpdate')
+expect(caseOnUpdate.data.caseStatus).toBe('OnUpdate')
+
+const caseWaitingForLawyer=await reviewers.changeStatus(caseID,'WaitingForLawyer')
+expect(caseWaitingForLawyer.data.caseStatus).toBe('WaitingForLawyer')
+
+const caseAssginedToLawyer=await reviewers.changeStatus(caseID,'AssginedToLawyer')
+expect(caseAssginedToLawyer.data.caseStatus).toBe('AssginedToLawyer')
+
+const caseWaitingForReviewer=await reviewers.changeStatus(caseID,'WaitingForReviewer')
+expect(caseWaitingForReviewer.data.caseStatus).toBe('WaitingForReviewer')
+
+const caseToReviewer=await reviewers.changeStatus(caseID,'AssginedToReviewer')
+expect(caseToReviewer.data.caseStatus).toBe('AssginedToReviewer')
+    
+try{
+       const caseTest=await reviewers.changeStatus(caseID,'habd')
+   }catch(err)
+   {
+
+   }
+const caseTest=await reviewers.getCase(caseID)
+expect(caseTest.data.caseStatus).not.toBe('habd')
+
+await reviewers.deleteCase(createdCase.data.data._id)
+await reviewers.deleteInvestor(createdInvestor.data._id)
+})
