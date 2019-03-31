d6c7f564c7550096fb753a31343ec431bd70be02
Mohamed Adel <moeadele98@gmail.com>
2019-03-31 17:10:38 +0200

    Added await

--- Jest/investors.js
+++ Jest/investors.js
@@ -2,25 +2,28 @@ const axios = require('axios')
 
 const investors = {
     default: async () => {
-        return axios.get('http://localhost:3000/api/investors/')
+        return await axios.get('http://localhost:3000/api/investors/')
     },
     registerInvestor: async (req) => {
-        return axios.post('http://localhost:3000/api/investors/register', req)
+        return await axios.post('http://localhost:3000/api/investors/register', req)
+    },
+    createInvestor: async(req) => {
+        return await axios.post('http://localhost:3000/api/investors', req)
     },
     viewComments: async (investorID, caseID) => {
-        return axios.get(`http://localhost:3000/api/investors/lawyerComments/${investorID}/${caseID}`)
+        return await axios.get(`http://localhost:3000/api/investors/lawyerComments/${investorID}/${caseID}`)
     },
     deleteInvestor: async(id) => {
-        return axios.delete(`http://localhost:3000/api/investors/${id}`)
+        return await axios.delete(`http://localhost:3000/api/investors/${id}`)
     },
     createCase: async (req)=>{
-        return axios.post('http://localhost:3000/api/cases', req)
+        return await axios.post('http://localhost:3000/api/cases', req)
     },
     deleteCase: async (id)=>{
-        return axios.delete(`http://localhost:3000/api/cases/${id}`)
+        return await axios.delete(`http://localhost:3000/api/cases/${id}`)
     },
     updateCase: async (id, req) => {
-        return axios.put(`http://localhost:3000/api/cases/update/${id}`, req)
+        return await axios.put(`http://localhost:3000/api/cases/update/${id}`, req)
     }
 }
 
