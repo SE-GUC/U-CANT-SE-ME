5378496237c8d6b94f4da7ce69d716a4795f3aeb
ZeyadKhattab <zeyad.khattab97@gmail.com>
2019-03-31 14:19:48 +0200

    added intital commit
    I did not fully test because the case upadte has some problems, i will continue when it is done

--- Jest/investors.test.js
+++ Jest/investors.test.js
@@ -11,4 +11,18 @@ test('As an investor I should be able to login', async() => {
     }
     const loginResult = await investors.login(loginInfo)
     return expect(loginResult.data.length).toBeGreaterThan(0)
-})  
+}) 
+test('As an investor I should be view my fees', async() => {
+    expect.assertions(1);
+    const investor= await investors.createInvestor();
+    const cas = await investors.createCase(investor._id);
+    let res= await investors.viewMyFees(investor._id);
+    
+    // await investors.changeStatus(cas._id);
+    // res= await investors.viewMyFees(investor._id);
+    
+    await investors.deleteCase(cas._id);
+    await investors.deleteInvestor(investor._id);
+    expect(res).toBe('you do not have any accepted company requests')
+
+}) 
