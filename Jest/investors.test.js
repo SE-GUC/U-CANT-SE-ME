55b0907e641e97262106798711113c5336c4f411
Mohamed Adel <moeadele98@gmail.com>
2019-03-31 17:17:29 +0200

    Added dependencies

--- Jest/investors.test.js
+++ Jest/investors.test.js
@@ -3,12 +3,41 @@
  */
 
 const investors = require('./investors')
+let createdEmail = ''
+let createdPassword = ''
+let investorId = ''
+let caseId = ''
+
+test('Create All Dependencies', async() => {
+    const investor = {
+        email:"moe@moe.moe",
+        password:"dontusethispassword",
+        fullName:"MoeMoeMoe",
+        type:"CEO",
+        gender:"Male",
+        nationality:"Egyptian",
+        methodOfIdentification:"National Card",
+        identificationNumber:"12233344445555",
+        dateOfBirth:"1997-12-15T22:00:00.000Z",
+        residenceAddress:"Rehab City",
+        telephoneNumber:"01007063067",
+        fax:"123456789"
+    }
+    createdInvestor  = await investors.registerInvestor(investor)
+    createdEmail = createdInvestor.data.data.email
+    createdPassword = investor.password
+    investorId = createdInvestor.data.data._id
+})    
 
 test('As an investor I should be able to login', async() => {
     const loginInfo = {
-        email: 'scrummaster@gmail.com',
-        password: '12345678fea'
+        email: createdEmail,
+        password: createdPassword
     }
     const loginResult = await investors.login(loginInfo)
     return expect(loginResult.data.length).toBeGreaterThan(0)
-})  
+}) 
+
+test('Delete All Dependencies', async () => {
+    await investors.deleteInvestor(investorId)
+})
