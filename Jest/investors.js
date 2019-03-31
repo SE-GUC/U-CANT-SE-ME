a7dadb25989a8aa789dfc1204f5c7548ce9c8b18
youssefdiaa <35878299+youssefdiaa@users.noreply.github.com>
2019-03-31 17:51:22 +0300

    Update investors.js

--- Jest/investors.js
+++ Jest/investors.js
@@ -2,22 +2,22 @@ const axios = require('axios')
 
 const investors = {
     login: async (loginInfo) => {
-        return axios.post('http://localhost:3000/api/investors/login', loginInfo)
+        return await axios.post('http://localhost:3000/api/investors/login', loginInfo)
     },
     trackMyCompany: async (id) =>{
-        return axios.get(`http://localhost:3000/api/investors/trackMyCompany/${id}`)
+        return await axios.get(`http://localhost:3000/api/investors/trackMyCompany/${id}`)
     },
     createCase: async (req) =>{
-        return axios.post('http://localhost:3000/api/cases/',req)
+        return await axios.post('http://localhost:3000/api/cases/',req)
     },
     deleteCase: async (id) =>{
-        return axios.delete(`http://localhost:3000/api/cases/${id}`)
+        return await axios.delete(`http://localhost:3000/api/cases/${id}`)
     },
     createInvestor: async (req) =>{
-        return axios.post('http://localhost:3000/api/investors/',req)
+        return await axios.post('http://localhost:3000/api/investors/',req)
     },
     deleteInvestor: async (id) =>{
-        return axios.delete(`http://localhost:3000/api/investors/${id}`)
+        return await axios.delete(`http://localhost:3000/api/investors/${id}`)
     }
 
 }
