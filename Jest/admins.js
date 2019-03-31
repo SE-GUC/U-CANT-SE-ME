2b748b18c1509ecc31fbc840d87f47b05b3a53e7
youssefdiaa <35878299+youssefdiaa@users.noreply.github.com>
2019-03-31 17:50:22 +0300

    Update admins.js

--- Jest/admins.js
+++ Jest/admins.js
@@ -2,19 +2,19 @@ const axios = require('axios')
 
 const admins = {
     default: async () => {
-        return axios.get('http://localhost:3000/api/admins/')
+        return await axios.get('http://localhost:3000/api/admins/')
     },
     readAdmin: async (id) =>{
-        return axios.get(`http://localhost:3000/api/admins/${id}`)
+        return await axios.get(`http://localhost:3000/api/admins/${id}`)
     },
     createAdmin: async (req)=>{
-        return axios.post('http://localhost:3000/api/admins/joi', req)
+        return await axios.post('http://localhost:3000/api/admins/joi', req)
     },
     deleteAdmin: async (id) => {
-        return axios.delete(`http://localhost:3000/api/admins/joi/${id}`)
+        return await axios.delete(`http://localhost:3000/api/admins/joi/${id}`)
     },
     updateAdmin: async (id,req) => {
-        return axios.put(`http://localhost:3000/api/admins/update/${id}`,req)
+        return await axios.put(`http://localhost:3000/api/admins/update/${id}`,req)
     }
 
 }
