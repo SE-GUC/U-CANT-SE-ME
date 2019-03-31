fe412652a988c0572ce81befbb6743d0c81aeb2f
ahmedshams99 <46957449+ahmedshams99@users.noreply.github.com>
2019-03-31 16:50:56 +0200

    Update investors.js

--- Jest/investors.js
+++ Jest/investors.js
@@ -2,16 +2,16 @@ const axios = require('axios')
 
 const investors = {
     default: async () => {
-        return axios.get('http://localhost:3000/api/investors/')
+        return await axios.get('http://localhost:3000/api/investors/')
     },
     registerInvestor: async (req) => {
-        return axios.post('http://localhost:3000/api/investors/register', req)
+        return await axios.post('http://localhost:3000/api/investors/register', req)
     },
     deleteInvestor: async(id) => {
-        return axios.delete(`http://localhost:3000/api/investors/${id}`)
+        return await axios.delete(`http://localhost:3000/api/investors/${id}`)
     },
     login: async (loginInfo) => {
-        return axios.post('http://localhost:3000/api/investors/login', loginInfo)
+        return await axios.post('http://localhost:3000/api/investors/login', loginInfo)
     }
 }
 
