cf004b1bc6480ac77cf4ad7b70efa5a07715b48d
ouda98 <47240156+ouda98@users.noreply.github.com>
2019-03-31 16:58:30 +0200

    await added

--- Jest/reviewers.js
+++ Jest/reviewers.js
@@ -2,19 +2,19 @@ const axios = require('axios')
 
 const reviewers = {
     default: async () => {
-        return axios.get('http://localhost:3000/api/reviewers/')
+        return await axios.get('http://localhost:3000/api/reviewers/')
     },
     readReviewer: async (id) =>{
-        return axios.get(`http://localhost:3000/api/reviewers/${id}`)
+        return await axios.get(`http://localhost:3000/api/reviewers/${id}`)
     },
     createReviewer: async (req)=>{
-        return axios.post('http://localhost:3000/api/reviewers/', req)
+        return await axios.post('http://localhost:3000/api/reviewers/', req)
     },
     deleteReviewer: async (id) => {
-        return axios.delete(`http://localhost:3000/api/reviewers/${id}`)
+        return await axios.delete(`http://localhost:3000/api/reviewers/${id}`)
     },
     updateReviewer: async (id,req) => {
-        return axios.put(`http://localhost:3000/api/reviewers/${id}`,req)
+        return await axios.put(`http://localhost:3000/api/reviewers/${id}`,req)
     }
 
 }
