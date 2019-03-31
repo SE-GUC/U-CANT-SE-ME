8c20eeacaf7bd33687347fc397c5ea82abc2c695
Mohamed Adel <moeadele98@gmail.com>
2019-03-31 22:57:23 +0200

    Added my user story.
    .............................................................

--- Jest/investors.test.js
+++ Jest/investors.test.js
@@ -268,26 +268,73 @@ let createdPassword = "";
 let investorId = "";
 let caseId = "";
 
-test("Create All Dependencies", async () => {
-  const investor = {
-    email: "moe@moe.moe",
-    password: "dontusethispassword",
-    fullName: "MoeMoeMoe",
-    type: "CEO",
-    gender: "Male",
-    nationality: "Egyptian",
-    methodOfIdentification: "National Card",
-    identificationNumber: "12233344445555",
-    dateOfBirth: "1997-12-15T22:00:00.000Z",
-    residenceAddress: "Rehab City",
-    telephoneNumber: "01007063067",
-    fax: "123456789"
-  };
-  createdInvestor = await investors.registerInvestor(investor);
-  createdEmail = createdInvestor.data.data.email;
-  createdPassword = investor.password;
-  investorId = createdInvestor.data.data._id;
-});
+test('Create All Dependencies', async() => {
+    const investor = {
+        email:"moemoe@moe.moe.com",
+        password:"dontusethispassword",
+        fullName:"MoeMoeMoeMoe",
+        type:"CEO",
+        gender:"Male",
+        nationality:"Egyptian",
+        methodOfIdentification:"National Card",
+        identificationNumber:"12233344445555",
+        dateOfBirth:"1997-12-15T22:00:00.000Z",
+        residenceAddress:"Rehab City",
+        telephoneNumber:"01007063067",
+        fax:"123456789"
+    }
+    createdInvestor  = await investors.createInvestor(investor)
+    createdEmail = createdInvestor.data.email
+    createdPassword = investor.password
+    investorId = createdInvestor.data._id
+    const mycase =  {
+        form: {
+            companyType: 'SPC',
+            regulatedLaw: 'lll',
+            legalFormOfCompany: 'NonProfit',
+            companyNameArabic: 'moeMan',
+            companyNameEnglish: 'Moe moooEEECompany Man',
+            headOfficeGovernorate: 'California',
+            headOfficeCity: 'San Francisco',
+            headOfficeAddress: '123st.',
+            phoneNumber: '01007063067',
+            fax: '987654321',
+            currencyUsedForCapital: 'EGP',
+            capital: 100
+        },
+        caseStatus: 'WaitingForLawyer',
+        creatorInvestorId: createdInvestor.data._id
+    }
+    const createdCase = await investors.createCase(mycase)
+    caseId = createdCase.data.data._id 
+    const updatedCase = {
+        form: {
+            companyType: 'SPC',
+            regulatedLaw: 'lll',
+            legalFormOfCompany: 'ManManNonProfitManTheManManMannnnnnnnnn',
+            headOfficeGovernorate: 'California',
+            headOfficeCity: 'San Francisco',
+            headOfficeAddress: '123st.',
+            phoneNumber: '01007063067',
+            fax: '987654321',
+            currencyUsedForCapital: 'EGP',
+            capital: 100
+        },
+        caseStatus: 'WaitingForLawyer',
+        comments:[
+            {
+                author:"Moe",
+                body:"Good Company!"
+                
+            },
+            {
+                author:"MoeMan",
+                body:"I second Moe"	
+            }
+        ]
+    }
+    await investors.updateCase(caseId, updatedCase)    
+})
 
 test("Registering an investor", async () => {
   const investor = {
@@ -326,9 +373,15 @@ test("As an investor I should be able to login", async () => {
   return expect(loginResult.data.length).toBeGreaterThan(0);
 });
 
-test("Delete All Dependencies", async () => {
-  await investors.deleteInvestor(investorId);
-});
+test('Viewing Lawyers Comments On My Case', async() => {
+    let comments = await investors.viewComments(investorId, caseId)
+    return expect(comments.data.comments.length).toBeGreaterThan(0)
+})
+
+test('Delete All Dependencies', async () => {
+    await investors.deleteInvestor(investorId)
+    await investors.deleteCase(caseId)
+})
 
 test("As an investor I should be view my fees", async () => {
   expect.assertions(3);
