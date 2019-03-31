1bd76ea917bf8e14202c893eb4f65f04067a276f
ZeyadKhattab <zeyad.khattab97@gmail.com>
2019-03-31 15:31:30 +0200

    added expects and tested update case

--- Jest/investors.test.js
+++ Jest/investors.test.js
@@ -13,16 +13,20 @@ test('As an investor I should be able to login', async() => {
     return expect(loginResult.data.length).toBeGreaterThan(0)
 }) 
 test('As an investor I should be view my fees', async() => {
-    expect.assertions(1);
+    expect.assertions(3);
     const investor= await investors.createInvestor();
     const cas = await investors.createCase(investor._id);
     let res= await investors.viewMyFees(investor._id);
     
-    // await investors.changeStatus(cas._id);
-    // res= await investors.viewMyFees(investor._id);
+    expect(res).toBe('you do not have any accepted company requests')
     
+    await investors.changeStatus(cas._id);
+    res= await investors.viewMyFees(investor._id);
     await investors.deleteCase(cas._id);
     await investors.deleteInvestor(investor._id);
-    expect(res).toBe('you do not have any accepted company requests')
-
+    
+    const name=res[0].companyName;
+    const fees=res[0].fees;
+    expect(fees).not.toBe(0);
+    expect(name).toEqual('DONTDELETE')
 }) 
