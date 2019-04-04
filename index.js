81e6795b1126a9ba700c996ec5112c3dd334090e
ZeyadKhattab <zeyad.khattab97@gmail.com>
2019-04-04 14:34:19 +0200

    ran commands to create template

--- index.js
+++ index.js
@@ -73,5 +73,5 @@ app.use((req, res) => {
   res.status(404).send({ err: "We can not find what you are looking for" });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
