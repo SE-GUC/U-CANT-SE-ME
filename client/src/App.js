1791a2e835ae100ea5fb6be1981b3add805725c7
Moe98 <moeadele98@gmail.com>
2019-04-04 22:00:00 +0200

    Using New Conventions

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
 import './App.css';
-import Comments from './components/Comments';
-import Header from './components/layout/Header'
-
+import Comments from './components/ViewComments/Comments'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router>
+        <div className="App">
         <Header />
-        <Comments />
-      </div>
+        <Route exact path="/" render={props => (
+          <React.Fragment>
+            <h1>Home</h1>
+          </React.Fragment>
+        )} />
+        <Route path="/viewComments" component={Comments}/>
+        </div>
+      </Router>
     );
   }
 }
-
 export default App;
--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
-    return (
-        <header style={headerStyle}>
-            <h1>Comments Section</h1>
-        </header>
-    )
+  return (
+    <header style={headerStyle}>
+      <h1>SUMERGE RCES</h1>
+      <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/viewComments">View Comments</Link>
+    </header>
+  )
 }
 
 const headerStyle = {
-    background: '#333',
-    color: '#fff',
-    textAlign: 'center',
-    padding: '10px'
+  background: '#333',
+  color: '#fff',
+  textAlign: 'center',
+  padding: '10px'
 }
 
-export default Header
+const linkStyle = {
+  color: '#fff',
+  textDecoration: 'none'
+}
+
+export default Header;
