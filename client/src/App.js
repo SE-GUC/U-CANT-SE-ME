04edd814d92827a57388daeba7295ac6d5f8f36f
Omar Samy <omarsamy90@gmail.com>
2019-04-04 21:53:40 +0200

    initial point

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import './App.css';
-import MyCompanies from './components/pages/MyCompanies';
 
 class App extends Component {
   render() {
@@ -15,11 +14,9 @@ class App extends Component {
             <h1>Home</h1>
           </React.Fragment>
         )} />
-        <Route path="/MyCompanies" component={MyCompanies}/>
         </div>
       </Router>
     );
   }
 }
-
 export default App;
--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header style={headerStyle}>
-      <h1>SUMERGE RCES</h1>
-      <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/MyCompanies">My Companies</Link>
+      <h1>SUMERGITE</h1>
+      <Link style={linkStyle} to="/">Home</Link> | 
     </header>
   )
 }
