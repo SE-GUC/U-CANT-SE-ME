a94358c005f4d0868dedc39f24e2f9e5e63aa36b
Ronic <abdoronic@gmail.com>
2019-04-07 00:08:36 +0200

    Changed the componentDidMount to not use async
    Changed the componentDidMount to use promises ( .then() ) not await to remove memory Leak warning

--- client/src/components/ViewMyFees/ViewMyFees.js
+++ client/src/components/ViewMyFees/ViewMyFees.js
@@ -1,27 +1,33 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import ViewMyFeesItem from './ViewMyFeesItem';
+import React, { Component } from "react";
+import axios from "axios";
+import ViewMyFeesItem from "./ViewMyFeesItem";
 
 class ViewMyFees extends Component {
-    state = {
-        fees: []
-    }
-    async componentDidMount() {
-        const investorID="5ca7594f3f074a35383a61a3"
-        // const investorID="5ca6229afd83c24bf091758e"
-        const {data: fees} = await axios.get(`http://localhost:5000/api/investors/viewMyFees/${investorID}/`)
-        this.setState({fees: fees.response})
-       }
-
-render(){
-   
-  return Array.isArray(this.state.fees)?
-  (
-      this.state.fees.map((fees) => (<ViewMyFeesItem key={fees.companyName} fees={fees} valid={true} />))):
-    (<ViewMyFeesItem  valid={false}  message={this.state.fees}/>)
+  state = {
+    fees: []
+  };
   
-    
-    }
+  componentDidMount() {
+    const investorID = "5ca7594f3f074a35383a61a3";
+    // const investorID="5ca6229afd83c24bf091758e"
+    // const { data: fees } = await axios.get(
+    //   `http://localhost:5000/api/investors/viewMyFees/${investorID}/`
+    // );
+    // this.setState({ fees: fees.response });
+    axios
+      .get(`http://localhost:5000/api/investors/viewMyFees/${investorID}/`)
+      .then(res => this.setState({ fees: res.data.response }));
+  }
+
+  render() {
+    return Array.isArray(this.state.fees) ? (
+      this.state.fees.map(fees => (
+        <ViewMyFeesItem key={fees.companyName} fees={fees} valid={true} />
+      ))
+    ) : (
+      <ViewMyFeesItem valid={false} message={this.state.fees} />
+    );
+  }
 }
 
 export default ViewMyFees;
--- client/src/components/ViewMyFees/ViewMyFeesItem.js
+++ client/src/components/ViewMyFees/ViewMyFeesItem.js
@@ -1,49 +1,44 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
 
 export class ViewMyFeesItem extends Component {
-    getStyle = () => {
-        return{
-            background: '#f4f4f4',
-            padding: '10px',
-            borderBottom: '1px #ccc dotted',
-        } 
-    }
-   companyNameStyle = () => {
-        return{
-            fontFamily: 'Century Gothic'
-        }
-    }
-    feesStyle = () => {
-        return{
-            background: '#f4f4f4',
-            padding: '10px',
-            fontFamily: 'Consolas'
-        }
-    }
-    
+  getStyle = () => {
+    return {
+      background: "#f4f4f4",
+      padding: "10px",
+      borderBottom: "1px #ccc dotted"
+    };
+  };
+  companyNameStyle = () => {
+    return {
+      fontFamily: "Century Gothic"
+    };
+  };
+  feesStyle = () => {
+    return {
+      background: "#f4f4f4",
+      padding: "10px",
+      fontFamily: "Consolas"
+    };
+  };
+
   render() {
-    const valid=this.props.valid;
-    if(valid){
-        const {companyName, fees} = this.props.fees;
-    return (
-      <div style={this.getStyle()}>
-        <h3 style={this.companyNameStyle()}>{companyName}</h3>
-        <h4 style={this.feesStyle()}>{fees}</h4>
-       </div>
-    )
-    
+    const valid = this.props.valid;
+    if (valid) {
+      const { companyName, fees } = this.props.fees;
+      return (
+        <div style={this.getStyle()}>
+          <h3 style={this.companyNameStyle()}>{companyName}</h3>
+          <h4 style={this.feesStyle()}>{fees}</h4>
+        </div>
+      );
+    } else {
+      return (
+        <div style={this.getStyle()}>
+          <h3 style={this.companyNameStyle()}>{this.props.message}</h3>
+        </div>
+      );
     }
-    else
-    {
-        return (
-            <div style={this.getStyle()}>
-              <h3 style={this.companyNameStyle()}>{this.props.message}</h3>
-             </div>
-          )
-    }
-    
   }
 }
 
-export default ViewMyFeesItem
+export default ViewMyFeesItem;
