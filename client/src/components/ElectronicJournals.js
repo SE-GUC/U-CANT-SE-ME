f2c75e6421746d7109f84246eb5af3c3f96274d0
youssefdiaa <35878299+youssefdiaa@users.noreply.github.com>
2019-04-12 00:06:29 +0300

    Update ElectronicJournals.js

--- client/src/components/ElectronicJournals.js
+++ client/src/components/ElectronicJournals.js
@@ -42,13 +42,13 @@ class ElectronicJournals extends Component {
         <p>
           {this.state.companies.map(company => {
             s[i] = ""
-            s[i] = s[i] + company.companyName + " company is a " + company.companyType + " company create at " + company.dateOfCreation
+            s[i] = s[i] + company.companyName + " company is a " + company.companyType + " company create in " + company.dateOfCreation
             i++
           })}
           {this.state.investors.map(investor => {
             s[j] = s[j] + ". The investor of the company is " + investor.fullName + " he is an " + investor.nationality + " investor, born in "
               + investor.dateOfBirth + " and lives in " + investor.residenceAddress + ". To contact him , you can use telephone " + investor.telephoneNumber
-              + " or fax " + investor.fax + " or email " + investor.email + " . The company is one of the companies established through Sumerge "
+              + " or fax " + investor.fax + " or email " + investor.email + " . The company is one of the companies established in Sumerge "
               + ", which combine innovation with solid technology expertise and deep business understanding to transform how you do business. They offer disruptive solutions that solve business challenges,"
               + " grow organizations, shape industries, empower your people and touch your customers’ lives. "
             j++
