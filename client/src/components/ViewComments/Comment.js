c6f2c553c1f2b774e90badc6fa40431fd880bb69
Moe98 <moeadele98@gmail.com>
2019-04-13 01:29:20 +0200

    Enhancements
    Forgot and reset password redirect to port 3000 instead of 5000 to make testing in the front-end easier
    Front-end of comments improved and changed

--- client/src/components/ViewComments/Comment.js
+++ client/src/components/ViewComments/Comment.js
@@ -1,45 +1,51 @@
 import React, { Component } from 'react'
 import moment from 'moment'
+import Avatar from '@material-ui/core/Avatar'
+import deepOrange from '@material-ui/core/colors/deepOrange'
+import deepPurple from '@material-ui/core/colors/deepPurple'
+import 'semantic-ui-css/semantic.min.css'
+import { Card } from 'semantic-ui-react'
 
 export class Comment extends Component {
-    getStyle = () => {
-        return{
-            background: '#f4f4f4',
-            padding: '10px',
-            borderBottom: '1px #ccc dotted',
-            textDecoration: this.props.comment.read? 'line-through': 'none'
-        } 
-    }
-    authorStyle = () => {
-        return{
-            fontFamily: 'Century Gothic'
-        }
-    }
-    commentStyle = () => {
-        return{
-            background: '#f4f4f4',
-            padding: '10px',
-            fontFamily: 'Consolas'
-        }
-    }
-    dateStyle = () => {
-        return{
-            fontFamily: 'Perpetua'
-        }
-    }
-
     formatTime(t) {
         return moment.utc(t.substring(0, 23)).format('DD, MMM, HH:mm').toUpperCase();
     }
-
-  render() {
-    const {author, body, date} = this.props.comment;
-    return (
-      <div style={this.getStyle()}>
-        <h3 style={this.authorStyle()}>{author}</h3>
-        <h4 style={this.commentStyle()}>{body}</h4>
-        <h5 style={this.dateStyle()}>{this.formatTime(date)}</h5>
-      </div>
+    
+    render() {
+        const styles = {
+            avatar: {
+              margin: 10,
+          },
+          orangeAvatar: {
+            margin: 10,
+            color: '#fff',
+            backgroundColor: deepOrange[500],
+            },
+          purpleAvatar: {
+            margin: 'auto',
+            color: '#fff',
+            backgroundColor: deepPurple[500],
+          },
+          author: {
+              margin: 'auto',
+              padding: '0px'
+          }
+        }
+        const {author, body, date} = this.props.comment
+        const avatarName = author.toString().charAt(0)
+        return (
+            <Card.Group itemsPerRow={1}>
+                <Card color='blue' header='Option 1'>
+                    <Card.Content header={author}>
+                        <Avatar style={styles.purpleAvatar}>{avatarName}</Avatar>
+                        {author}
+                    </Card.Content>
+                    <Card.Content description={body} />
+                    <Card.Content extra>
+                        {this.formatTime(date)}
+                    </Card.Content>
+                </Card>
+            </Card.Group>
     )
   }
 }
