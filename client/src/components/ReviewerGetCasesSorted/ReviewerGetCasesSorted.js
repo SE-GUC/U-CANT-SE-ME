06ff49208a23ddd6df69f2fd0a098f6eac7dcb92
fadi-saad <fadoxin@gmail.com>
2019-04-13 16:16:19 +0200

    changed sorting to descending if on date

--- client/src/components/ReviewerGetCasesSorted/ReviewerGetCasesSorted.js
+++ client/src/components/ReviewerGetCasesSorted/ReviewerGetCasesSorted.js
@@ -12,12 +12,12 @@ class ReviewerGetCasesSorted extends Component {
         };
     }
     
-    async componentDidMount(){
-        if(this.state.criterion === 'ID'){
+    async componentDidMount(criteria){
+        if(criteria === 'ID'){
             const getCases = await axios.get('http://localhost:5000/api/reviewers/getMyCasesByid/5ca612f6dc10782330077c4e');
             this.setState({cases: getCases.data});
         }else{
-            if(this.state.criterion === 'Date'){
+            if(criteria === 'Date'){
                 const getCases = await axios.get('http://localhost:5000/api/reviewers/getMyCasesByDate/5ca612f6dc10782330077c4e');
                 this.setState({cases: getCases.data});
             }
@@ -26,7 +26,7 @@ class ReviewerGetCasesSorted extends Component {
 
     changeCriterion= e => {
         this.setState({criterion: e.target.value})
-        this.componentDidMount()
+        this.componentDidMount(e.target.value)
     }
 
     render() {
@@ -34,8 +34,9 @@ class ReviewerGetCasesSorted extends Component {
             <div>
                 <label>Selection Criterion: </label>
                 <select onChange={this.changeCriterion}>
-                    <option value="ID">Case ID</option>
-                    <option value="Date">Date Of Creation</option>
+                    <option value=""></option>
+                    <option value="ID">Case ID / Date Of Creation Ascending</option>
+                    <option value="Date">Date Of Creation Descending</option>
                 </select>
                 {this.state.cases.map((x) => (
                 <Case key={x._id} case={x} />
