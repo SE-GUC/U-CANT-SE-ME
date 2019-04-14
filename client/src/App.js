20cc3f66b87a426acdde630490d28ee16951189b
Moe98 <moeadele98@gmail.com>
2019-04-14 21:11:26 +0200

    Added profile types
    Co-Authored-By: ahmedshams99 <ahmedshams99@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,10 @@ import ReviewerViewTasks from './components/caseComponents/ReviewerViewTasks';
 import LawyerViewTasks from './components/caseComponents/LawyerViewTasks';
 import Forgot from './components/forgotAndReset/forgot'
 import Reset from './components/forgotAndReset/reset'
-
+import InvestorProfile from './components/InvestorProfile'
+import LawyerProfile from './components/LawyerProfile'
+import ReviewerProfile from './components/ReviewerProfile'
+import AdminProfile from './components/AdminProfile'
 class App extends Component {
   render() {
     return (
@@ -102,6 +105,10 @@ class App extends Component {
         <Route path="/LawyerViewTasks" component={LawyerViewTasks}/>
         <Route path="/forgot" component={Forgot}/>
         <Route path="/:type/reset/:token" component={Reset}/>
+        <Route path="/profile/:id" component={InvestorProfile}/>
+        <Route path="/internalPortal/lawyer/profile/:id" component={LawyerProfile}/>
+        <Route path="/internalPortal/reviewer/profile/:id" component={ReviewerProfile}/>
+        <Route path="/internalPortal/admin/profile/:id" component={AdminProfile}/>
         </div>
       </Router>
     );
