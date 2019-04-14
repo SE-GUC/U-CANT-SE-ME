a6c2c2bbe3f004a87f5d609f429f58c669b65535
Ronic <abdoronic@gmail.com>
2019-04-14 23:55:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -48,6 +48,14 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
+
 //Homepage
 app.get("/", (req, res) => res.send("HomePage"));
 
@@ -78,14 +86,6 @@ app.use("/api/externalEntities", externalEntities);
 app.use((req, res) => {
   res.status(404).send({ err: "We can not find what you are looking for" });
 });
-
-const path = require("path");
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  });
-}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
