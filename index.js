e83b8d6708eb475bdee32113080f880a8a8f7a24
Ronic <abdoronic@gmail.com>
2019-04-15 19:47:53 +0200

    Deploy trial 7

--- index.js
+++ index.js
@@ -52,7 +52,12 @@ app.use(passport.session());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "client", "build", "index.html"),
+      err => {
+        res.send(err);
+      }
+    );
   });
 }
 
