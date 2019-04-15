b3c88fdf0d6835296d3780d441a563104c7f567e
Ronic <abdoronic@gmail.com>
2019-04-15 15:20:30 +0200

    Deploy 3

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -14,29 +14,6 @@ function Header() {
     | <Link style={linkStyle} to="/login">Login</Link>
     | <Link style={linkStyle} to="/InvestorRegister">Register Investor</Link>
     | <Link style={linkStyle} to="/ElectronicJournals">Electronic Journals</Link>
-    | <Link style={linkStyle} to="/getAllCases">Cases</Link>	
-    | <Link style={linkStyle} to="/RegisterLawyer">Register Lawyer</Link> 	
-    | <Link style={linkStyle} to="/RegisterReviewer">Register Reviewer</Link> 	
-    | <Link style={linkStyle} to="/LawyerFillForm">Lawyer Fill Form</Link>	
-    | <Link style={linkStyle} to="/InvestorFillForm">Investor Fill Form</Link>	
-    | <Link style={linkStyle} to="/lawyerUpdateCase">lawyerUpdateCase</Link>	
-    | <Link style={linkStyle} to="/ElectronicJournals">Electronic Journals</Link>	
-    | <Link style={linkStyle} to="/LastLawyer">LastLawyer</Link>	
-    | <Link style={linkStyle} to="/login">Login</Link>	
-    | <Link style={linkStyle} to="/LawyerViewCase">LawyerViewCase</Link>	
-    | <Link style={linkStyle} to="/ReviewerViewCase">ReviewerViewCase</Link>	
-    | <Link style={linkStyle} to="/InvestorUpdateCase">InvestorUpdateCase</Link>	
-    | <Link style={linkStyle} to="/CasesSummary">CasesSummary</Link>	
-    | <Link style={linkStyle} to="/CaseSwitch">CaseSwitch</Link>	
-    | <Link style={linkStyle} to="/LawyerViewAllCases">LawyerViewAllCases</Link>	
-    | <Link style={linkStyle} to="/ReviewerViewAllCases">ReviewerViewAllCases</Link>	
-    | <Link style={linkStyle} to="/AdminViewAllCases">AdminViewAllCases</Link>	
-    | <Link style={linkStyle} to="/LawyerGetCasesSorted">Lawyer Get Cases Sorted</Link>	
-    | <Link style={linkStyle} to="/ReviewerGetCasesSorted">Reviewer Get Cases Sorted</Link>	
-    | <Link style={linkStyle} to="/ReviewerViewTasks">ReviewerViewTasks</Link>	
-    | <Link style={linkStyle} to="/LawyerViewTasks">LawyerViewTasks</Link>	
-    | <Link style={linkStyle} to="/updateInvestorProfile">updateInvestorProfile</Link>	
-    | <Link style={linkStyle} to="/CreateFormTemplate">CreateFormTemplate</Link>
     </header>
   )
 }
