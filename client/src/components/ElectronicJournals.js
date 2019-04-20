26479bb87a0e212bf4d12fb7135afbd918a5f5f3
youssefdiaa <youssefdiaa.ingame18@gmail.com>
2019-04-20 12:31:00 +0300

    d

--- client/src/components/ElectronicJournals.js
+++ client/src/components/ElectronicJournals.js
@@ -6,13 +6,14 @@ class ElectronicJournals extends Component {
     super(props)
     this.state = {
       companies: [],
-      investors: []
+      investors: [],
+      journals: []
     }
   }
 
   async componentDidMount() {
 
-    const res = await axios.get(`api/companies`);
+    const res = await axios.get(`http://localhost:5000/api/companies`);
 
     const { data: companies } = res
 
@@ -21,41 +22,44 @@ class ElectronicJournals extends Component {
     const investorsArray = []
     for (var i = 0; i < res.data.data.length; i++) {
 
-      const investorID = this.state.companies[i].investorID
+      const investorID = this.state.companies[i].investorId
 
-      const res = await axios.get(`api/investors/${investorID}`)
+      const res = await axios.get(`http://localhost:5000/api/investors/${investorID}`)
 
       investorsArray.push(res.data)
     }
 
     this.setState({ investors: investorsArray });
 
+      var s = [""]
+    var i = 0
+    var j = 0
+    {this.state.companies.map(company => {
+      s[i] = ""
+      s[i] = s[i] + company.companyNameArabic + " company is a " + company.companyType + " company create in " + company.dateOfCreation
+      i++
+    })}
+    {this.state.investors.map(investor => {
+      s[j] = s[j] + ". The investor of the company is " + investor.fullName + " he/she is an " + investor.nationality + " investor, born in "
+        + investor.dateOfBirth + ". To contact him/her , you can use telephone " + investor.telephoneNumber
+        + " or fax " + investor.fax + " or email " + investor.email + " .  "
+      
+      j++
+    })}
+    this.setState({ journals: s });
+    
 
   }
 
   render() {
-    var s = [""]
-    var i = 0
-    var j = 0
+
     return (
       <ul>
         <p>
-          {this.state.companies.map(company => {
-            s[i] = ""
-            s[i] = s[i] + company.companyName + " company is a " + company.companyType + " company create in " + company.dateOfCreation
-            i++
-          })}
-          {this.state.investors.map(investor => {
-            s[j] = s[j] + ". The investor of the company is " + investor.fullName + " he/she is an " + investor.nationality + " investor, born in "
-              + investor.dateOfBirth + ". To contact him/her , you can use telephone " + investor.telephoneNumber
-              + " or fax " + investor.fax + " or email " + investor.email + " .  "
-            
-            j++
-          })}
+          
+          {this.state.journals.map(z => {
 
-          {s.map(z => {
-
-            return <div>
+            return <div style={this.getStyle()}>
               <ul style={this.lineStyle()}
                 key={z}>{z}
               </ul>
@@ -65,13 +69,21 @@ class ElectronicJournals extends Component {
           })}
 
 
-          {this.state.companies._id}
+        
         </p>
       </ul>
     )
 
   }
 
+  getStyle = () => {
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+
+    }
+  }
   lineStyle = () => {
     return {
       background: '#f4f4f4',
