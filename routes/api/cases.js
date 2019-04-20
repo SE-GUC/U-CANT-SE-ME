bc88ea5d34b7491e197f7fb10bd4bec6318fc048
Moe98 <moeadele98@gmail.com>
2019-04-20 13:30:17 +0200

    added authentication for cases
    Co-Authored-By: Yahia Badr <yahiabadr@users.noreply.github.com>
    Co-Authored-By: Zeyad Khattab <zeyadkhattab@users.noreply.github.com>

--- routes/api/cases.js
+++ routes/api/cases.js
@@ -2,17 +2,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
-
+const passport = require("passport")
 const caseController = require("../../controllers/caseController");
-
+const allAuth = passport.authenticate(['adminAuth','lawyerAuth','reviewerAuth'],{session: false});
 //get all cases
-router.get("/", caseController.getAllCases);
+router.get("/",allAuth, caseController.getAllCases);
 
 //get certain case
 router.get("/:id", caseController.getCase);
 
 //create case
-router.post("/", caseController.createCase);
+router.post("/",allAuth, caseController.createCase);
 
 //update
 router.put("/:id", caseController.updateCase);
