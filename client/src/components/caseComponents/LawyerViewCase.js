801d45ef7187dfa3c61ab3b9b05e597cb2f106e5
yahiabadr <yahya.hisham97@gmail.com>
2019-04-20 14:56:36 +0200

    add an alert
    Co-Authored-By: Zeyad Khattab <zeyadkhattab@users.noreply.github.com>

--- client/src/components/caseComponents/LawyerViewCase.js
+++ client/src/components/caseComponents/LawyerViewCase.js
@@ -6,9 +6,31 @@ export default class LawyerViewCase extends Component {
     state ={
         cases :[],
         caseid:"",
-        lawyerID:""
+        lawyerID:"",
+        home:false
     };
+    async componentDidMount() {
+        // check if localStorage.jwtTokenis not null else he must login
+        if (!localStorage.jwtToken) {
+          alert("You must login!");
+          this.setState({ home: true });
+        }
+        try{
+            await axios.get('api/admins/auth')
+        }catch(err){this.setState({ home: true });}
+        // check that it is a reviewer if not redirect to somewhere else
+      }
     async componentDidMount(){
+        if (!localStorage.jwtToken) {
+            alert("You must login!");
+            this.setState({ home: true });
+        }
+        try{
+              await axios.get('api/admins/auth')
+        }catch(err){
+            alert("You are not allowed to access this page");
+            this.setState({ home: true });
+        }
         const data = parseJwt(localStorage.jwtToken)
         await this.setState({lawyerID:data.id})
         const id =this.state.lawyerID;
