bd231ac9293ad00d61913ffd48f7d0b3ec8c7a57
yahiabadr <yahya.hisham97@gmail.com>
2019-04-21 22:18:08 +0200

    Create AdminDashBoard.js
    Co-Authored-By: Zeyad Khattab <zeyadkhattab@users.noreply.github.com>

---
+++ client/src/components/DashBoard/AdminDashBoard.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+// Be sure to include styles at some point, probably during your bootstraping
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+import SideNav, {NavItem,NavIcon,NavText} from "@trendmicro/react-sidenav";
+import { Redirect } from 'react-router-dom'
+export default class InvestorDashBoard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            id: '',
+            caseSummary: false,
+            caseSwitch: false,
+            viewAllCases: false,
+            registerLawyer: false,
+            registerReviewer: false,
+            createFormTemplate: false,
+            home:0
+        }
+      }
+    handleSelect = selected => {
+    console.log(selected);
+    if(selected === "viewmycompanies")
+    {
+        this.setState({registerLawyer: true})
+    }else
+    if (selected === "case/all") {
+
+    }
+  };
+  render() {
+    const styles = {
+      iconStyle: {
+        fontSize: "1.75em"
+      },
+      navStyle: {
+        background: "#3d58db",
+        zindex:"0"
+      },
+      divStyleShow:{
+        display: ' inline',
+        textAlign: "center"
+        // marginLeft:"64px",
+        // backgroundColor:"black"
+      },
+      divStyleHide:{
+        display: 'none'
+      }
+    };
+    return (
+      <div>
+          {
+            this.state.caseSummary? <Redirect to={{pathname: "/CasesSummary"}}/>:
+            this.state.caseSwitch? <Redirect to={{pathname: "/CaseSwitch"}}/>:
+            this.state.viewAllCases? <Redirect to={{pathname: "/AdminViewAllCases"}}/>:
+            this.state.registerLawyer? <Redirect to={{pathname: "/RegisterLawyer"}}/>:
+            this.state.registerReviewer? <Redirect to={{pathname: "/RegisterReviewer"}}/>:
+            this.state.createFormTemplate? <Redirect to={{pathname: "/CreateFormTemplate"}}/>:<div/>
+        }
+        <SideNav onSelect={this.handleSelect} style={styles.navStyle}>
+          <SideNav.Toggle />
+          <SideNav.Nav defaultSelected={this.props.defaultSelected}>
+            
+            <NavItem eventKey="createformtemplate">
+              <NavIcon>
+                <i className="fa fa-home" style={styles.iconStyle} />
+              </NavIcon>
+              <NavText>Create From Template</NavText>
+            </NavItem>
+            
+            <NavItem eventKey="register">
+              <NavIcon>
+                <i className="fa fa-list-alt" style={styles.iconStyle} />
+              </NavIcon>
+              <NavText>Register</NavText>
+              <NavItem eventKey="createnewcompany/fillform">
+                <NavText>Register Lawyer</NavText>
+              </NavItem>
+              <NavItem eventKey="createnewcompany/lawyer">
+                <NavText>Register Reviewer</NavText>
+              </NavItem>
+            </NavItem>
+
+            <NavItem eventKey="viewallcases">
+              <NavIcon>
+                <i className="fa fa-home" style={styles.iconStyle} />
+              </NavIcon>
+              <NavText>View All Cases</NavText>
+            </NavItem>            
+
+          </SideNav.Nav>
+        </SideNav>
+      </div>
+    );
+  }
+}
