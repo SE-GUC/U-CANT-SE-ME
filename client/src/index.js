084d83051c32246334854f59fe6b6d8e034c7a8c
yahiabadr <yahya.hisham97@gmail.com>
2019-04-22 15:09:35 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import InvestorDashBoard from './components/DashBoard/InvestorDashBoard'
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<InvestorDashBoard />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<InvestorD/ashBoard />, document.getElementById('root'));
