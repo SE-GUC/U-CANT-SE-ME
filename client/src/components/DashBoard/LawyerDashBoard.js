0da33d3a431cc1009bd5f92af083760cd661291f
Moe98 <moeadele98@gmail.com>
2019-04-23 06:37:36 +0200

    LawyerDashBoard Arabic
    Co-Authored-By: Mohamed Mantawy <b1ackmanta@users.noreply.github.com>
    Co-Authored-By: ahmedshams99 <ahmedshams99@users.noreply.github.com>
    Co-Authored-By: Zeyad Khattab <zeyadkhattab@users.noreply.github.com>
    Co-Authored-By: Yahia Badr <yahiabadr@users.noreply.github.com>

--- client/src/components/DashBoard/LawyerDashBoard.js
+++ client/src/components/DashBoard/LawyerDashBoard.js
@@ -1,40 +1,44 @@
 import React, { Component } from "react";
 // Be sure to include styles at some point, probably during your bootstraping
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";
-import SideNav, {NavItem,NavIcon,NavText} from "@trendmicro/react-sidenav";
-import { Redirect } from 'react-router-dom'
+import SideNav, { NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
+import { Redirect } from "react-router-dom";
 import LawyerViewTasks from "../caseComponents/LawyerViewTasks";
 import LawyerViewCase from "../caseComponents/LawyerViewCase";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import CaseSwitch from "../caseComponents/CaseSwitch";
 import CasesContainer from "../dCaseComponents/CasesContainer";
 import { red } from "@material-ui/core/colors";
-import NavBarDashboard from '../NavBarDashboard'
+import NavBarDashboard from "../NavBarDashboard";
 export default class LawyerDashBoard extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-        dashboardwidth:0
-    }
+      dashboardwidth: 0,
+      lang: ""
+    };
   }
-async componentDidMount(){
-  const width = document.getElementById("dashboard").clientWidth
-  await this.setState({dashboardwidth:width});
-}
-    handleSelect = selected => {
+  async componentDidMount() {
+    if (localStorage.getItem("lang"))
+      this.setState({ lang: localStorage.getItem("lang") });
+    else this.setState({ lang: "eng" });
+    const width = document.getElementById("dashboard").clientWidth;
+    await this.setState({ dashboardwidth: width });
+  }
+  handleSelect = selected => {
     console.log(selected);
-    document.getElementById("UnassignedCases").style.display="none";
-    document.getElementById("Tasks").style.display="none";
-    document.getElementById("AllCases").style.display="none";
+    document.getElementById("UnassignedCases").style.display = "none";
+    document.getElementById("Tasks").style.display = "none";
+    document.getElementById("AllCases").style.display = "none";
 
     if (selected === "viewunasignedcases")
-      document.getElementById("UnassignedCases").style.display=null;
+      document.getElementById("UnassignedCases").style.display = null;
 
     if (selected === "viewtasks")
-      document.getElementById("Tasks").style.display=null;
+      document.getElementById("Tasks").style.display = null;
 
     if (selected === "viewallcases")
-        document.getElementById("AllCases").style.display=null;
+      document.getElementById("AllCases").style.display = null;
   };
   render() {
     const styles = {
@@ -43,63 +47,86 @@ async componentDidMount(){
       },
       navStyle: {
         background: "#3480E3",
-        position:"fixed"
+        position: "fixed"
       },
-      divStyleShow:{
-        marginLeft:this.state.dashboardwidth,
-        paddingTop:'10vh'
+      divStyleShow: {
+        marginLeft: this.state.dashboardwidth,
+        paddingTop: "10vh"
       },
-      divStyleHide:{
-        display: 'none',
-        marginLeft:this.state.dashboardwidth,
-        paddingTop:'10vh'
+      divStyleHide: {
+        display: "none",
+        marginLeft: this.state.dashboardwidth,
+        paddingTop: "10vh"
       }
     };
     return (
       <div>
-        <NavBarDashboard sumergiteColor= '#3480E3' boxShadow='0px 3px 20px rgba(0, 0, 0, 0.16)' dashboard='bold' profile='lighter' homepage='lighter' DASHBOARD={true} PROFILE={true} ProfileMargin='120px' HomePageMargin='0px'  dashboardRedirect='/LawyerDashBoard' profileRedirect="/internalPortal/lawyer/profile"/> 
-        <SideNav id="dashboard" onSelect={this.handleSelect} style={styles.navStyle}>
+        <NavBarDashboard
+          sumergiteColor="#3480E3"
+          boxShadow="0px 3px 20px rgba(0, 0, 0, 0.16)"
+          dashboard="bold"
+          profile="lighter"
+          homepage="lighter"
+          DASHBOARD={true}
+          PROFILE={true}
+          ProfileMargin="120px"
+          HomePageMargin="0px"
+          dashboardRedirect="/LawyerDashBoard"
+          profileRedirect="/internalPortal/lawyer/profile"
+        />
+        <SideNav
+          id="dashboard"
+          onSelect={this.handleSelect}
+          style={styles.navStyle}
+        >
           <SideNav.Toggle />
           <SideNav.Nav defaultSelected="viewtasks">
-            
-          <NavItem eventKey="viewtasks">
+            <NavItem eventKey="viewtasks">
               <NavIcon>
                 <a className="fa fa-list-alt" style={styles.iconStyle} />
               </NavIcon>
-              <NavText>View Tasks</NavText>
+              <NavText>
+                {this.state.lang === "eng" ? "View Tasks" : "أظهر مهماتي"}
+              </NavText>
             </NavItem>
 
             <NavItem eventKey="viewunasignedcases">
               <NavIcon>
                 <a className="fa fa-list-alt" style={styles.iconStyle} />
               </NavIcon>
-              <NavText>View Unasigned Cases</NavText>
+              <NavText>
+                {this.state.lang === "eng"
+                  ? "View Unassigned Cases"
+                  : "أظهر العمليات المتاحة"}
+              </NavText>
             </NavItem>
 
             <NavItem eventKey="viewallcases">
               <NavIcon>
                 <a className="fa fa-home" style={styles.iconStyle} />
               </NavIcon>
-              <NavText>View All Cases</NavText>
-            </NavItem>  
-
+              <NavText>
+                {this.state.lang === "eng"
+                  ? "View All Cases"
+                  : "أظهر جميع العمليات"}
+              </NavText>
+            </NavItem>
           </SideNav.Nav>
         </SideNav>
 
-        <div id="UnassignedCases" style={styles.divStyleHide} >
-        <Router>
-          <LawyerViewCase/>
+        <div id="UnassignedCases" style={styles.divStyleHide}>
+          <Router>
+            <LawyerViewCase />
           </Router>
         </div>
-        <div id="Tasks" style={styles.divStyleShow} >
-        <Router>
-          <LawyerViewTasks/>
+        <div id="Tasks" style={styles.divStyleShow}>
+          <Router>
+            <LawyerViewTasks />
           </Router>
         </div>
-        <div id="AllCases" style={styles.divStyleHide} >
-          <CasesContainer/>
+        <div id="AllCases" style={styles.divStyleHide}>
+          <CasesContainer />
         </div>
-
       </div>
     );
   }
